9757d42950db86bab10ef64e9e0240e7c0d0e3fb
Dimitar Georgiev <divanovg94@gmail.com>
2021-03-31 09:09:09 +0300

    Added Creating Book method

---
+++ BookListRazor/Migrations/20210330081824_AddISBNToBookModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListRazor.Migrations
+{
+    public partial class AddISBNToBookModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Author",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ISBN",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ISBN",
+                table: "Book");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Author",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -15,6 +15,8 @@ namespace BookListRazor.Model
         [Required]
         public string Name { get; set; }
 
-        public int Author { get; set; }
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
     }
 }
